413d21c0a767a53349e67b92396b109355599950
r10nw7fd3 <snr1s@outlook.com>
2023-06-12 18:57:29 +0300

    Add % sign

--- panel.sh
+++ panel.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 while true; do
-	xsetroot -name " \"$(wpa_cli status | grep -E "^ssid=" | cut -d = -f 2)\" $(date +'%d.%m.%Y %H:%M') $(pactl get-sink-volume @DEFAULT_SINK@ | awk '{print $5}' | tr -d '\n') $(cat /sys/class/power_supply/BAT1/capacity) "
+	xsetroot -name " \"$(wpa_cli status | grep -E "^ssid=" | cut -d = -f 2)\" $(date +'%d.%m.%Y %H:%M') $(pactl get-sink-volume @DEFAULT_SINK@ | awk '{print $5}' | tr -d '\n') $(cat /sys/class/power_supply/BAT1/capacity)% "
 	sleep 1
 done
