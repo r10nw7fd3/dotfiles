3b1b15f17a4a2c7333c41c392ee8d5a3e950d384
r10nw7fd3 <r10nw7fd3@outlook.com>
2024-04-14 12:31:28 +0300

    Update panel

--- panel.sh
+++ panel.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 
 while true; do
-	xsetroot -name " \"$(wpa_cli status | grep -E "^ssid=" | cut -d = -f 2)\" $(date +'%d.%m.%Y %H:%M') $(pactl get-sink-volume @DEFAULT_SINK@ | awk '{print $5}' | tr -d '\n') $(cat /sys/class/power_supply/BAT1/capacity)% "
+	wpa_state=$(wpa_cli status) | grep wpa_state=COMPLETED
+	if [ "$(wpa_cli status | grep wpa_state=COMPLETED)" = "wpa_state=COMPLETED" ]; then
+		wpa_state="+"
+	else
+		wpa_state="-"
+	fi
+	date=$(date +"%d.%m.%Y %H:%M")
+	volume=$(pactl get-sink-volume @DEFAULT_SINK@ | awk '{print $5}' | tr -d '\n')
+	bat_cap=$(cat /sys/class/power_supply/BAT1/capacity)%
+	xsetroot -name " $wpa_state $date $volume $bat_cap "
 	sleep 1
 done
